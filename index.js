7912f76fb375f4b5f215dc90c11db74cf90be455
Kenn Su <kliusu1412@gmail.com>
2024-11-09 17:56:03 -0500

    completes the lab tests for array filter method lab, maybe think about how to clean up the code

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function(drivers) {return drivers.toLowerCase() === name.toLowerCase()});
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(function(drivers) {
+        return drivers.slice(0, name.length) === name});
+}
+
+function matchName(drivers, item) {
+    return drivers.filter(function(drivers) {
+        return drivers.name === item});
+}
